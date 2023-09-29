c01089540d494ae2c11724dd60187e24da47445c
Michael McCool <michael.mccool@intel.com>
2023-09-29 14:49:21 -0300

    Update lab1.py

--- mitdeeplearning/lab1.py
+++ mitdeeplearning/lab1.py
@@ -5,18 +5,6 @@ import urllib
 import numpy as np
 import tensorflow as tf
 
-# This should be made conditional for Intel Dev Cloud, already defined in Google Colab
-from IPython.core.magic import (register_line_magic)
-
-@register_line_magic
-def tensorflow_version(line):
-    "tensorflow_version"
-    return line
-
-# In an interactive session, we need to delete it after registration to avoid
-# name conflicts for automagic to work on line magics.
-del tensorflow_version
-
 from IPython.display import Audio
 
 
