5f3107e9d1fb8b8ce120df744da617fa43af165e
Michael McCool <michael.mccool@intel.com>
2023-11-03 20:30:05 -0300

    Update abc2mid.sh

--- mitdeeplearning/bin/abc2mid.sh
+++ mitdeeplearning/bin/abc2mid.sh
@@ -2,8 +2,8 @@
 
 abcfile=$1
 suffix=${abcfile%.abc}
-$HOME/.local/bin/abc2midi $abcfile -o "$suffix.mid"
+$HOME/.local/bin/abc2midi $abcfile -o "$suffix.mid" 2> "$suffix.err.txt"
+grep Error "$suffix.err.txt"
 code=$?
-echo "Return code of abc2midi is $code"
 # rm "$suffix.abc"
 exit $code
