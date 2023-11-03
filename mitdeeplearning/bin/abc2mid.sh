7ade2a9e80c95c7fe4ac2d7ae9c3551ff9a5b78a
Michael McCool <michael.mccool@intel.com>
2023-11-03 20:34:57 -0300

    Update abc2mid.sh

--- mitdeeplearning/bin/abc2mid.sh
+++ mitdeeplearning/bin/abc2mid.sh
@@ -3,7 +3,8 @@
 abcfile=$1
 suffix=${abcfile%.abc}
 $HOME/.local/bin/abc2midi $abcfile -o "$suffix.mid" 2> "$suffix.err.txt"
+# note that grep returns 0 if there is a match, so we have to negate it
 grep Error "$suffix.err.txt"
 code=$?
 # rm "$suffix.abc"
-exit $code
+exit [ "$code" -eq 0 ]
