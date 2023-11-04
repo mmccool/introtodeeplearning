411ac0d770d9ea184e8d86519b20eddf483fff2a
Michael McCool <michael.mccool@intel.com>
2023-11-03 21:44:37 -0300

    Update abc2mid.sh

--- mitdeeplearning/bin/abc2mid.sh
+++ mitdeeplearning/bin/abc2mid.sh
@@ -2,5 +2,17 @@
 
 abcfile=$1
 suffix=${abcfile%.abc}
-$HOME/.local/bin/abc2midi $abcfile -o "$suffix.mid"
-rm "$suffix.abc"
+# stupid abc2midi program does not return normal error exit codes or use stderr for errors, argh argh argh
+$HOME/.local/bin/abc2midi $abcfile -o "$suffix.mid" > "$suffix.err.txt"
+# ... so scan output for any errors
+grep Error "$suffix.err.txt"
+code=$?
+# echo "grep Exit code $code"
+# rm "$suffix.abc"
+# note that grep returns 0 if there is a match, it succeeds if there is an error, so we have to negate it
+if [ "$code" -eq "0" ]
+then
+        exit 1
+else
+        exit 0
+fi
